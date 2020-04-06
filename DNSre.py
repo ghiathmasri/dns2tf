48ac599c5a783cd75aba74be1c862a6d33dd689d
Ghiath Masri <ghiathmasri@Ghiaths-MBPro.local>
2020-04-06 08:13:56 -0500

    add pattern

--- DNSre.py
+++ DNSre.py
@@ -1,6 +1,7 @@
 import re
 
 fileName = "db.rnstg.com"
+TTL = "3600"
 
 Value = re.compile(r'(@)|(\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b)')
 Type = re.compile(r'(?<=IN\s)(\w*)')
@@ -10,10 +11,10 @@ with open (fileName, 'r') as f:
     for line in f:
         try: 
             ValueMatche = Value.search(line).group()
-            print(ValueMatche)
+            # print(ValueMatche)
             try: 
                 TypeMatche = Type.search(line).group()
-                print(TypeMatche)
+                # print(TypeMatche)
             except: pass
             try: 
                 PointToMatches = PointTo.finditer(line)
@@ -22,9 +23,53 @@ with open (fileName, 'r') as f:
                         pass
                     else:
                         PointToMatche = PointToMatche.group()
-                        print(PointToMatche)
+                        # print(PointToMatche)
                         break
             except: pass
+            # print (f'{ValueMatche}\t{TypeMatche}\t{PointToMatche}')
+
+            print (
+                f"resource" "aws_route53_record" "support_aa-dining_com" "{"
+                    "zone_id" "=" "aws_route53_zone.aa-dining_com.zone_id"
+                    "name" "=" "support.aa-dining.com"
+                    "type" "=" "CNAME"
+                    "ttl"  "=" "3600"
+                    "records" "=" "[""aa-dining.zendesk.com""]"
+                "}"
+            )
+
+
+
+
+
+
+
+
         except: pass
-        
-     
+
+
+
+# resourceName = 
+# zone_id = aws_route53_{resourceName}.zone_id
+# name = "{ValueMatche}"
+# RTtype = "{TypeMatche}"
+# records = "{PointToMatche}"
+# ttl = "{TTL}"
+
+
+
+
+
+# resource "aws_route53_zone" "aa-dining_com" {
+#   name = "aa-dining.com"
+#   tags = {"Name":"aa-dining.com", "DNS":"External", "Resource":"DNS"}
+#   comment = "Managed by TF"
+# }
+
+# resource "aws_route53_record" "support_aa-dining_com" {
+#   zone_id = aws_route53_zone.aa-dining_com.zone_id
+#   name    = "support.aa-dining.com"
+#   type    = "CNAME"
+#   ttl     = "3600"
+#   records = ["aa-dining.zendesk.com"]
+# }
