a69217a115ccdaefa7aea075a8a645667021031e
Ghiath Masri <51256494+ghiathmasri@users.noreply.github.com>
2020-04-06 23:54:30 -0500

    Update DNSre.py

--- DNSre.py
+++ DNSre.py
@@ -1,13 +1,43 @@
 import re
+import sys
 
-fileName = "db.rnstg.com"
+if len(sys.argv) < 2:
+    print("Usage:\n{0} <zonefile>".format(sys.argv[0]))
+    exit(1)
+
+def _gen_zone(**d):
+    return """
+resource "aws_route53_zone" "{root_zone_name}" {{
+  name = "{root_zone}"
+}}""".format(
+        **d
+    )
+
+
+def _gen_record(**d):
+    return """
+resource "aws_route53_record" "{record_name}" {{
+  zone_id = aws_route53_zone.{root_zone_name}.zone_id
+  name    = "{name}"
+  type    = "{type}"
+  records = ["{record}"]
+  ttl     = {ttl}
+}}""".format(
+        **d
+    )
+
+fileName = sys.argv[1]
 TTL = "3600"
 
-Value = re.compile(r'(@)|(\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b)')
-Type = re.compile(r'(?<=IN\s)(\w*)')
-PointTo = re.compile(r'(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})|(?<=(\"))(\b.*\b)|((\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b))')
+Value = re.compile(r"(@)|((\b|\*.)((?=[a-z0-9-_]{1,63}\.)(xn--)?[a-z0-9_\*]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b)|(^\w*\b)")
+Type = re.compile(r"(?<=IN\s)(\w*)|([Mm][Xx])|([Ss][Rr][Vv])")
+PointTo = re.compile(r"(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})|(?<=(\"))(\b.*\b)|((\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b))")
+Priority = re.compile(r"(?<=[Mm][Xx]\s)(\d*)|(?<=[Ss][Rr][Vv]\s)(\d*)")
+Port_Or_Weight = re.compile(r"(\b\d{1,5}\b)")
 
 with open (fileName, 'r') as f:
+    root_zone = ""
+    root_zone_name = ""
     for line in f:
         try: 
             ValueMatche = Value.search(line).group()
@@ -26,17 +56,61 @@ with open (fileName, 'r') as f:
                         # print(PointToMatche)
                         break
             except: pass
-            # print (f'{ValueMatche}\t{TypeMatche}\t{PointToMatche}')
+            try:
+                PriorityMatch = Priority.search(line).group()
+            except:
+               pass
+            try:
+                Port_Or_Weight_Match = Port_Or_Weight.finditer(line)
+                next(Port_Or_Weight_Match)
+                Port = next(Port_Or_Weight_Match).group()
+                Weight = next(Port_Or_Weight_Match).group()
+            except: pass
+            if TypeMatche == "SOA":
+                root_zone = ValueMatche
+                root_zone_name = root_zone.replace('.','_')
 
-            print (
-                f"resource" "aws_route53_record" "support_aa-dining_com" "{"
-                    "zone_id" "=" "aws_route53_zone.aa-dining_com.zone_id"
-                    "name" "=" "support.aa-dining.com"
-                    "type" "=" "CNAME"
-                    "ttl"  "=" "3600"
-                    "records" "=" "[""aa-dining.zendesk.com""]"
-                "}"
-            )
+                print (_gen_zone(root_zone=root_zone,
+                        root_zone_name=root_zone_name))
+            
+            elif TypeMatche == "A" or TypeMatche == "CNAME" or TypeMatche == "TXT":
+                print (_gen_record(record_name='{0}-{1}'.format(ValueMatche.replace('.','_').replace('*','_'),PointToMatche.lower().replace('.','_').replace('=','_').replace('+','_').replace(':','_').replace(' ', '')),
+                    name=ValueMatche,
+                    ttl=TTL,
+                    type=TypeMatche,
+                    record=PointToMatche,
+                    root_zone_name=root_zone_name))
+            elif TypeMatche == "MX":
+                PointToMatche_MX = f'{PriorityMatch} {PointToMatche}'
+                print (_gen_record(record_name='{0}-{1}'.format(ValueMatche.replace('.','_'),PointToMatche.lower().replace('.','_')),
+                    name=ValueMatche,
+                    ttl=TTL,
+                    type=TypeMatche,
+                    record=PointToMatche_MX,
+                    root_zone_name=root_zone_name))
+            elif TypeMatche == "SRV":
+                PointToMatche_SRV = f'{PriorityMatch} {Weight} {Port} {PointToMatche}'
+                print (_gen_record(record_name='{0}-{1}'.format(ValueMatche.replace('.','_'),PointToMatche.lower().replace('.','_')),
+                    name=ValueMatche,
+                    ttl=TTL,
+                    type=TypeMatche,
+                    record=PointToMatche_SRV,
+                    root_zone_name=root_zone_name))
+            else:
+                print (_gen_record(record_name='{0}-{1}'.format(ValueMatche.replace('.','_'),PointToMatche.lower().replace('.','_')),
+                    name=ValueMatche,
+                    ttl=TTL,
+                    type=TypeMatche,
+                    record=PointToMatche,
+                    root_zone_name=root_zone_name))
+            # if TypeMatche == "MX":
+            #     print(f"{ValueMatche}\t{TypeMatche}\t{PriorityMatch}\t{PointToMatche}")
+            # elif TypeMatche == "SRV":
+            #     print(
+            #         f"{ValueMatche}\t{TypeMatche}\t{PriorityMatch}\t{Weight}\t{Port}\t{PointToMatche}"
+            #     )
+            # else:
+            #     print(f"{ValueMatche}\t{TypeMatche}\t{PointToMatche}")
 
 
 
@@ -47,7 +121,7 @@ with open (fileName, 'r') as f:
 
         except: pass
 
-
+       
 
 # resourceName = 
 # zone_id = aws_route53_{resourceName}.zone_id
